d808e779ec8911755b1e9d1b893e69019838e372
Emmanuel Apau <etapau@gmail.com>
2023-02-01 08:48:49 -0500

    default txt

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pyqrcode
 
-url = "https://mechanicode.io/careers.html"
+# url = "https://mechanicode.io/careers-qr.html"
+url = "https://mechanicode.io"
 
 code = pyqrcode.create(url)
 
 # code.png('code.png', scale=6)
 # Inverts the color to background and white code
-code.png('code.png', scale=6, module_color='FFFFFF', background='000000')
+# code.png('code.png', scale=6, module_color='FFFFFF', background='000000')
+
+code.png('code.png', scale=6)
